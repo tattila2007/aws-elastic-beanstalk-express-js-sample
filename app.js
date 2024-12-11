5fe30ab43b880780bb31a1337924eec0118e45e7
tattila2007 <104862885+tattila2007@users.noreply.github.com>
2024-12-11 22:25:57 +0200

    second

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Bello!'));
+app.get('/', (req, res) => res.send('Hello BelloBello!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
